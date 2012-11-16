20f3a6b7846e84823e0d7f8b3792bac80f21b124
andersonrodrigues7@gmail.com <andersonrodrigues7@gmail.com@27e3f231-4525-b78e-c459-a7bfd4e808f2>
2012-11-16 14:40:46 +0000

    gerarGrafo

---
+++ trunk/PDS - FB/src/apifb/GerarGrafo.java
@@ -0,0 +1,107 @@
+package apifb;
+
+import graph.AbstractGraph;
+import graph.ConcreteEdge;
+import graph.ConcreteVertex;
+import graph.Edge;
+import graph.GraphAsList;
+import graph.Vertex;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import tests.ParDeVerticesNaoExistenteException;
+import tests.VerticeJaExisteException;
+
+public class GerarGrafo {
+
+	List<String> usuarios = new ArrayList<>();
+	List<String> enderecos = new ArrayList<>();
+
+	AbstractGraph graph = new GraphAsList();
+
+	BufferedReader in_ids;
+	BufferedReader in_nomes;
+	BufferedReader in_aniversarios;
+
+	public GerarGrafo() {
+
+		usuarios.add("felipecordeiroalves");
+		usuarios.add("larissabatistaleite");
+		usuarios.add("showrodrigues");
+
+		for (int i = 0; i < usuarios.size(); i++) {
+			enderecos.add("/home/felipe/ids_" + usuarios.get(i) + ".txt");
+			enderecos.add("/home/felipe/nomes_" + usuarios.get(i) + ".txt");
+			enderecos.add("/home/felipe/aniversarios_" + usuarios.get(i)
+					+ ".txt");
+		}
+	}
+
+	/* Transforma string em data. */
+	public static Data process(String str) throws IOException {
+
+		Data data = new Data();
+
+		if (!str.equals("null")) {
+			data.setDia(str.substring(0, 2));
+			data.setMes(str.substring(3, 5));
+			if (!str.substring(5).equals("")) {
+				data.setAno(str.substring(6, 10));
+			}
+
+		}
+
+		return data;
+
+	}
+
+	/* Povoa o grafo */
+	private void Povoar() throws IOException, VerticeJaExisteException,
+			ParDeVerticesNaoExistenteException, FileNotFoundException {
+		
+		for (int i = 0; i < enderecos.size();) {
+
+			in_ids = new BufferedReader(new FileReader(enderecos.get(i++)));
+			in_nomes = new BufferedReader(new FileReader(enderecos.get(i++)));
+			in_aniversarios = new BufferedReader(new FileReader(
+					enderecos.get(i++)));
+
+			boolean primeiraLinha = true;
+			String id = in_ids.readLine();
+			Vertex user = null;
+
+			while (in_ids.ready()) {
+				if (primeiraLinha) {
+					ConcreteVertex vertex = new ConcreteVertex(
+							in_nomes.readLine(),
+							process(in_aniversarios.readLine()), id);
+					graph.addVertex(vertex);
+					user = graph.searchVertex(id);
+					primeiraLinha = false;
+				} else {
+					ConcreteVertex vertex = new ConcreteVertex(
+							in_nomes.readLine(),
+							process(in_aniversarios.readLine()),
+							in_ids.readLine());
+					graph.addVertex(vertex);
+					Edge edge = new ConcreteEdge(user,
+							graph.searchVertex(vertex.getId()));
+					graph.addEdge(edge);
+				}
+			}
+		}
+
+		graph.setAdjacencyList(graph.getEdges());
+
+	}
+	
+	public AbstractGraph getGraph() throws FileNotFoundException, IOException, VerticeJaExisteException, ParDeVerticesNaoExistenteException{
+		Povoar();
+		return graph;
+	}
+}
