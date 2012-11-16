a3a3f30d29dcf60817dc3351f690c2010ca10435
andersonrodrigues7@gmail.com <andersonrodrigues7@gmail.com@27e3f231-4525-b78e-c459-a7bfd4e808f2>
2012-11-16 14:40:46 +0000

    gerarGrafo

--- PDS - FB/src/apifb/NomeNiver.java
+++ PDS - FB/src/apifb/NomeNiver.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import tests.ParDeVerticesNaoExistenteException;
+import tests.VerticeJaExisteException;
 import visitor.VisitaAniversario;
 import visitor.Visitor;
 
@@ -14,97 +16,31 @@ import graph.*;
 
 public class NomeNiver {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException,
+			ParDeVerticesNaoExistenteException, VerticeJaExisteException {
 
 		List<String> usuarios = new ArrayList<>();
-		List<String> enderecos = new ArrayList<>();
-
-		usuarios.add("felipecordeiroalves");
-		usuarios.add("larissabatistaleite");
-		usuarios.add("showrodrigues");
+		GerarGrafo gerador = new GerarGrafo();
 
 		CriarArquivoAniversarios arq_aniversario = new CriarArquivoAniversarios();
 		CriarArquivoIds arq_ids = new CriarArquivoIds();
 		CriarArquivoNomes arq_nomes = new CriarArquivoNomes();
 		Visitor visitor = new VisitaAniversario();
 
-		
+		usuarios.add("felipecordeiroalves");
+		usuarios.add("larissabatistaleite");
+		usuarios.add("showrodrigues");
 
-	for (int i = 0; i < usuarios.size(); i++) {
-//			arq_ids.criarArquivoIds(usuarios.get(i));
-			//arq_nomes.criarArquivoNomes(usuarios.get(i));
-			//arq_aniversario.criarArquivoAniversarios(usuarios.get(i));
-		enderecos.add("/home/felipe/ids_" + usuarios.get(i) + ".txt");
-		enderecos.add("/home/felipe/nomes_" + usuarios.get(i) + ".txt");
-		enderecos.add("/home/felipe/aniversarios_" + usuarios.get(i)
-				+ ".txt");
-	}
+		for (int i = 0; i < usuarios.size(); i++) {
+			// arq_ids.criarArquivoIds(usuarios.get(i));
+			// arq_nomes.criarArquivoNomes(usuarios.get(i));
+			// arq_aniversario.criarArquivoAniversarios(usuarios.get(i));
 
-		AbstractGraph graph = new GraphAsList();
-
-		BufferedReader in_ids;
-		BufferedReader in_nomes;
-		BufferedReader in_aniversarios;
-
-		for (int i = 0; i < enderecos.size();) {
-
-			try {
-
-				in_ids = new BufferedReader(new FileReader(enderecos.get(i++)));
-				in_nomes = new BufferedReader(
-						new FileReader(enderecos.get(i++)));
-				in_aniversarios = new BufferedReader(new FileReader(
-						enderecos.get(i++)));
-
-				boolean primeiraLinha = true;
-				String id = in_ids.readLine();
-				Vertex user = null;
-				
-				while (in_ids.ready()) {
-					if (primeiraLinha) {
-						ConcreteVertex vertex = new ConcreteVertex(
-								in_nomes.readLine(),
-								process(in_aniversarios.readLine()), id);
-						graph.addVertex(vertex);
-						user = graph.searchVertex(id);
-						primeiraLinha = false;
-					} else {
-						ConcreteVertex vertex = new ConcreteVertex(
-								in_nomes.readLine(),
-								process(in_aniversarios.readLine()),
-								in_ids.readLine());
-						graph.addVertex(vertex);
-						Edge edge = new ConcreteEdge(user, graph.searchVertex(vertex.getId()));
-						graph.addEdge(edge);
-					}
-				}
-
-			} catch (FileNotFoundException e1) {
-				e1.printStackTrace();
-			}
 		}
-		
-		graph.setAdjacencyList(graph.getEdges());
-//		graph.depthFirstTraversal(visitor, 0);
+		AbstractGraph graph = gerador.getGraph();
+
+		// graph.depthFirstTraversal(visitor, 0);
 		graph.breadthFirstTraversal(visitor, 0);
-
-
-	}
-
-	public static Data process(String str) throws IOException {
-
-		Data data = new Data();
-
-		if (!str.equals("null")) {
-			data.setDia(str.substring(0, 2));
-			data.setMes(str.substring(3, 5));
-			if (!str.substring(5).equals("")) {
-				data.setAno(str.substring(6, 10));
-			}
-
-		}
-
-		return data;
 
 	}
 }
